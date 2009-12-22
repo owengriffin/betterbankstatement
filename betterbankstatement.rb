16c1b9ef76c8aaea7f50895e28da5f0cfd92ebe6
Owen Griffin <ogriffin@precisionm90.(none)>
2009-12-22 13:56:01 +0000

    Modified root script

--- betterbankstatement.rb
+++ betterbankstatement.rb
@@ -1,53 +1,20 @@
-require './lib/betterbankstatement.rb'
-require 'lib/openstreetmap.rb'
-
-# puts OpenStreetMap::Geocoder.find_location('Reading').inspect
+#!/usr/bin/ruby
+# betterbankstatement.rb <folder>
+# <folder> = A folder containing bank statements
+require 'lib/betterbankstatement.rb'
 
 parser = BetterBankStatement::Parser.new
-transactions=[]
-Dir.foreach("statements") { |filename|
+Dir.foreach(ARGV[0]) { |filename|
   if filename =~ /.*\.txt$/
-    transactions = transactions + parser.open("statements/#{filename}")
+    parser.open("#{ARGV[0]}/#{filename}")
+  elsif filename =~ /.*\.qif$/
+    parser.open_qif("#{ARGV[0]}/#{filename}")
+  elsif filename =~ /.*\.csv/
+    parser.open_csv("#{ARGV[0]}/#{filename}")
   end
 }
-
-# Dir.foreach("bankaccount") { |filename|
-#    if filename =~ /.*\.csv$/
-#      transactions = transactions + parser.open_csv("bankaccount/#{filename}")
-#    end
-#  }
-
-Dir.foreach("bankaccount") { |filename|
-  if filename =~ /.*\.qif$/
-    transactions = transactions + parser.open_qif("bankaccount/#{filename}")
-  end
-}
-
-# BetterBankStatement::Account.all.each { |account|
-# puts account.inspect
-# }
-
-#transactions = parser.open_xhb("bankaccount/homebank.xhb")
 
 BetterBankStatement::Payee.load_filters("filters.yaml")
 BetterBankStatement::Payee.categorize_all
-
-# BetterBankStatement::Graph.category_barchart
-# BetterBankStatement::Graph.category_piechart
-
-# BetterBankStatement::Payee.all.each {|payee|
-#   puts "Payee: #{payee.name}"
-#   payee.transactions.each { |transaction|
-#     puts "   #{transaction.date} #{transaction.description}"
-#   }
-# }
-
-# BetterBankStatement::Location.all.each {|location|
-#   puts "Location #{location.name}"
-# }
-
-BetterBankStatement::Category.all.each { |category|
-  puts "#{category.name} #{category.transactions.length} #{category.total_amount}"
-}
 
 BetterBankStatement::Statement.generate
